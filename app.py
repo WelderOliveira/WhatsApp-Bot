92c5504996cbe3a7fb04c22c3481fd68b92cd624
WelderOliveira <weldersilva326@gmail.com>
2023-06-13 00:59:01 -0300

    Atualizando chaves WB

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from selenium import webdriver
 import time
 import pandas as pd
 import urllib
+
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-msg = urllib.parse.quote("Isso é um Teste! Esse texto está sendo escrito pelo Robô Boladão \\n Você é uma Cobaia %0D Liga Noix qualquer coisa %0D Ihuuuuu %0D Isso ainda é um teste!")
+msg = "asdasdasdasd!"
+
 contatos_df = pd.read_excel(r"C:\Users\Will\Downloads\ContatosEnvia.xlsx", usecols=[0], names=["Numeros"])
 cont = int(len(contatos_df.index))
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://web.whatsapp.com')
 
-while len(driver.find_elements_by_id("side")) < 1:
-    time.sleep(1)
+while len(driver.find_elements(By.ID, "side")) < 1:
+    time.sleep(5)
+
 
 def buscar_contato(contatos):
-    campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    campo_pesquisa = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p')
     time.sleep(4)
     campo_pesquisa.click()
     campo_pesquisa.send_keys(contatos)
@@ -24,14 +28,20 @@ def buscar_contato(contatos):
     campo_pesquisa.send_keys(Keys.ENTER)
     time.sleep(5)
 
+
 def enviar_mensagem(msg):
-    campo_msg = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
-    campo_msg[1].click()
-    time.sleep(3)
-    campo_msg[1].send_keys(msg)
-    time.sleep(5)
-    campo_msg[1].send_keys(Keys.ENTER)
+    campo_msg = driver.find_element(By.XPATH,
+                                    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+    campo_msg.click()
+    time.sleep(7)
+    for m in msg:
+        if m == "&":
+            campo_msg.send_keys(Keys.SHIFT + Keys.ENTER)
+        else:
+            campo_msg.send_keys(m)
+    campo_msg.send_keys(Keys.ENTER)
     time.sleep(10)
+
 
 for num in range(cont):
     if num != ' ':
